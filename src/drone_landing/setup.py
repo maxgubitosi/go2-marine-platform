6ef749b9b4af4ffa2825b48c8e522dd35c929c8f
Máximo Gubitosi <mgubitosi@udesa.edu.ar>
2025-12-28 13:30:04 -0300

    add simple drone

---
+++ src/drone_landing/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+
+package_name = 'drone_landing'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*.xacro')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='linar',
+    maintainer_email='mgubitosi@udesa.edu.ar',
+    description='Drone landing simulation with downward-facing camera',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'drone_controller = drone_landing.drone_controller:main',
+        ],
+    },
+)
